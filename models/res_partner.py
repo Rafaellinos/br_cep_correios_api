567cdb26892076e025f31451f25591476704d809
Rafael Lino <rafael.veloso.lino@hotmail.com>
2019-10-14 09:50:14 -0300

    Fix search and add replace

--- models/res_partner.py
+++ models/res_partner.py
@@ -20,15 +20,14 @@ class ResPartner(models.Model):
     @api.onchange('zip')
     def search_cep(self):
         if self.zip:
-            if len(self.zip) == 8:
-                #raise Warning(str(dados))
-                #cep = '03072000'
-                url_api = ('https://viacep.com.br/ws/%s/json' % self.zip)
+            zip_last = self.zip.replace('-','').replace(';','').replace('--','').replace(')','').replace('(','')
+            if len(zip_last) == 8:
+                url_api = ('https://viacep.com.br/ws/%s/json' % zip_last)
                 response = requests.get(url_api)
                 if response.status_code == 200:
                     dict_api = json.loads(response.text)
-                    #raise Warning(dict_api['logradouro'])
-                    state = self.env['res.country.state'].search([('code','=', dict_api['uf'])], limit=1)
+                    br_country = self.env['res.country'].search([('name','=', 'Brasil')], limit=1)
+                    state = self.env['res.country.state'].search([('code','=', dict_api['uf']),('country_id','=',br_country.id)], limit=1)
                     if state:
                         self.state_id = state.id
                     self.street = dict_api['logradouro']
